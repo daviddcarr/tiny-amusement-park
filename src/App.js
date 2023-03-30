4809dc0a053708b46dc46837c325603354ff94b8
daviddcarr <daviddcarr13@gmail.com>
2023-03-30 18:27:59 -0500

    Add loading fallback.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import './App.css'
+import { Suspense } from 'react'
 import { Canvas } from "@react-three/fiber"
 
 import Scene from "./components/Scene"
 import Main from "./components/ui/Main"
 import CameraWrapper from './components/CameraWrapper'
+import Loading from './components/ui/Loading'
 
 function App() {
   return (
-    <div className="app w-100 h-screen">
-      <Canvas
-        shadows
-        camera={{ 
-          position: [12, 5, 0],
-          near: 0.01,
-        }}
-        >
-        <CameraWrapper />
-        <Scene />
-      </Canvas>
-      <Main />
-    </div>
+    <Suspense fallback={<Loading />} >
+      <div className="app w-100 h-screen">
+        <Canvas
+          shadows
+          camera={{ 
+            position: [12, 5, 0],
+            near: 0.01,
+          }}
+          >
+          <CameraWrapper />
+          <Scene />
+        </Canvas>
+        <Main />
+      </div>
+    </Suspense>
   )
 }
 
