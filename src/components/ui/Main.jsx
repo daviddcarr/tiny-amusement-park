9abf8c24e73c5f26512cfd9b795fff0f07b5a897
daviddcarr <daviddcarr13@gmail.com>
2023-03-30 13:57:15 -0500

    Update meta info, favicon, and add info screen.

--- src/components/ui/Main.jsx
+++ src/components/ui/Main.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react"
 
+import {
+    TbInfoHexagon
+} from 'react-icons/tb'
+
 import useCameraState from "../../hooks/useCameraState"
 
 export default function Main() {
 
     const [hasInteracted, setHasInteracted] = useState(false)
+    const [showInfo, setShowInfo] = useState(false)
 
     const [
         isRidingRide,
@@ -37,7 +42,35 @@ export default function Main() {
                     >
                     Exit
                 </button> }
+                <button
+                    className={`${showInfo ? 'bg-red-400' : 'bg-gray-700'} px-4 py-2 rounded-lg text-white uppercase text-bold hover:bg-red-500 pointer-events-auto`}
+                    onClick={() => setShowInfo(!showInfo)}
+                    >
+                    <TbInfoHexagon className="h-[24px] w-[24px]" />
+                </button>
             </div>
+
+            { showInfo &&
+                <div className="absolute w-full max-w-lg top-[50px] left-1/2 -translate-x-1/2 space-x-4 p-4 flex items-center pointer-events-auto">
+                    <div className="bg-gray-700 bg-opacity-90 p-4 rounded-lg">
+                        <h1 className="text-2xl text-white font-bold mb-4">Three.js Tiny Park!</h1>
+
+                        <p className="text-white mb-4">
+                            This is a small demo of a 3D park built with Three.js and React.
+                        </p>
+
+                        <p className="text-white mb-4">
+                            Tap any of the rides to enter a POV!
+                        </p>
+
+                        <p className="text-white mb-4">
+                            Check out more of my work at <a href="https://daviddylancarr.com" target="_blank" className="text-blue-400 hover:text-blue-200">daviddylancarr.com</a>
+                        </p>
+                    </div>
+                </div>
+            }
+
+
         </div>
     )
 }
